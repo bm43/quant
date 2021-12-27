a1c06613b1e84a62dd2c04bc24d0604464374413
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2021-12-27 23:37:26 +0900

    Update feature_importance.py

--- adv_in_fin_ML/feature_importance.py
+++ adv_in_fin_ML/feature_importance.py
@@ -94,22 +94,6 @@ def auxFeatImpSFI(featNames,clf,trnsX,cont,scoring,cvGen):
         imp.loc[featName,'std']=df0.std()*df0.shape[0]**-0.5
     return imp
 
-# generate random data and features
-def getTestData(n_features=40, n_informative=10, n_redundant=10, n_samples=100):
-
-    from sklearn.datasets import make_classification # make random dataset for classification
-    trnsX, cont = make_classification(n_samples=n_samples, n_features=n_features,\
-    n_informative=n_informative, n_redundant=n_redundant, random_state=0, shuffle=False)
-    # trnsX = data, cont=label
-    df0 = pd.date_range(end=datetime.today(), periods=n_samples, freq='B')
-    trnsX = pd.DataFrame(trnsX, index=df0)
-    cont = pd.Series(cont, index=df0).to_frame('bin')
-    df0 = ['I_'+str(i) for i in range(n_informative)]+['R_'+str(i) for i in range(n_redundant)]
-    df0 += ['N_'+str(i) for i in range(n_features-len(df0))]
-    trnsX.columns = df0
-    cont['w'] = 1./cont.shape[0]
-    cont['t1'] = pd.Series(cont.index, index=cont.index)
-    return trnsX, cont
 
 # snippet 7.3 p.109
 from sklearn.model_selection._split import _BaseKFold
